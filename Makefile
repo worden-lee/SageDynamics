36589150afceeb1486e4f3c6f9d78acb72e59602
Lee Worden <worden.lee@gmail.com>
2016-01-22 11:49:04 -0800

    changing to python package structure

---
+++ Makefile
@@ -0,0 +1,4 @@
+# to install the python package into sage.  possibly needs to be run sudo.
+# you may prefer to use setup.py develop if you'll be editing in these files
+install:
+	sage -python setup.py install
---
+++ setup.py
@@ -0,0 +1,12 @@
+# http://python-packaging.readthedocs.org/en/latest/minimal.html
+from setuptools import setup
+
+setup(name='dynamicalsystems',
+      version='0.1',
+      description='Sage classes for dynamical systems',
+      url='http://github.com/worden-lee/SageDynamics',
+      author='Lee Worden',
+      author_email='worden.lee@gmail.com',
+      license='GPL 2.0',
+      packages=['dynamicalsystems'],
+      zip_safe=False)
